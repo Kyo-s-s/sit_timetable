5dcfc2ae5797f767c7928b97e202c95b2ecd138a
Kyo-s-s <igagrid407@gmail.com>
2023-03-10 23:35:20 +0900

    [Add] credits

--- src/TimeTable/TimeTableCredit.tsx
+++ src/TimeTable/TimeTableCredit.tsx
@@ -1,30 +1,55 @@
 import React from 'react';
-import { Lecture } from './Lecture';
+import { Container, Row, Col, Table } from 'react-bootstrap';
+import { Lecture, lectureNone } from './Lecture';
 
 type Credits = { [key: string]: number };
 
 export const TimeTableCredit = (lectures: Lecture[], selectedLecture: Lecture[][]) => {
 
-  let _credits: Credits = {};
+  let credits: Credits = {};
+  let sum = 0;
   for (let lecture of lectures) {
-    _credits[lecture.category] = 0;
+    if (lecture.name === lectureNone.name) continue;
+    credits[lecture.category] = 0;
   }
 
   for (let i = 0; i < selectedLecture.length; i++) {
     for (let j = 0; j < selectedLecture[i].length; j++) {
-      if (selectedLecture[i][j].name !== "") {
-        _credits[selectedLecture[i][j].category] += selectedLecture[i][j].credit;
+      if (selectedLecture[i][j].name !== lectureNone.name) {
+        credits[selectedLecture[i][j].category] += selectedLecture[i][j].credit;
+        sum += selectedLecture[i][j].credit;
       }
     }
   }
 
   return (
     <>
-    {
-      Object.keys(_credits).map((key, _) => {
-        return <p>{key}: {_credits[key]}</p>
-      })
-    }
+      <Container>
+        <Row>
+          <Table style={{textAlign: "right"}}>
+            <thead>
+              <tr>
+                {
+                  Object.keys(credits).map((key, _) => {
+                    return <th>{key}</th>
+                  })
+                }
+                <th>総和</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                {
+                  Object.keys(credits).map((key, _) => {
+                    return <td>{credits[key]}</td>
+                  })
+                }
+                <td>{sum}</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Row>
+      </Container>
     </>
   )
 }
