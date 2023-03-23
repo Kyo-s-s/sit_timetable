1a750429ebac46e1b4bc50d7a9c207691fd0f772
Kyo-s-s <igagrid407@gmail.com>
2023-03-23 14:51:45 +0900

    [Fix] script.js

--- script.js
+++ script.js
@@ -1,59 +1,104 @@
-const subjectList = Array.from(document.querySelectorAll("#Subject"));
-const classList = subjectList.filter((sub) => sub.querySelector("table") != undefined);
+// http://timetable.sic.shibaura-it.ac.jp/
 
-const lectures = classList.map((cls) => {
-    const table = Array.from(cls.querySelectorAll("table td"));
-    const texts = table[3].innerHTML.split("<br>");
+const lectureData = (element) => {
+    const subjectList = Array.from(element.querySelectorAll("#Subject"));
+    const classList = subjectList.filter((sub) => sub.querySelector("table") != undefined);
 
-    let weekParent = cls.parentElement;
-    while ([0, 1].some(idx => weekParent.children[idx].attributes.bgcolor.value != "#FFFFCC")) {
-        weekParent = weekParent.previousElementSibling;
-    }
-    const week = weekParent.children[0].innerText;
+    const lectures = classList.map((cls) => {
+        const table = Array.from(cls.querySelectorAll("table td"));
+        const texts = table[3].innerHTML.split("<br>");
 
-    const period = Number(cls.attributes.row.value) + 1;
-    const credit = Number(texts[1].slice(0, 1));
+        let weekParent = cls.parentElement;
+        while ([0, 1].some(idx => weekParent.children[idx].attributes.bgcolor.value != "#FFFFCC")) {
+            weekParent = weekParent.previousElementSibling;
+        }
+        const week = weekParent.children[0].innerText;
 
-    let divisionParent = cls.parentElement;
-    while (Array.from(divisionParent.children).every(item => item.attributes.bgcolor.value != "#FFFFCC")) {
-        divisionParent = divisionParent.previousElementSibling;
-    }
+        const period = Number(cls.attributes.row.value) + 1;
+        const credit = Number(texts[1].slice(0, 1));
 
-    let division;
-    if (divisionParent.children[1].attributes.bgcolor.value == "#FFFFCC") {
-        division = divisionParent.children[1].innerText;
-    } else {
-        division = divisionParent.children[0].innerText;
-    }
+        let divisionParent = cls.parentElement;
+        while (Array.from(divisionParent.children).every(item => item.attributes.bgcolor.value != "#FFFFCC")) {
+            divisionParent = divisionParent.previousElementSibling;
+        }
 
-    const time = Number(cls.attributes.colspan.value);
+        let division;
+        if (divisionParent.children[1].attributes.bgcolor.value == "#FFFFCC") {
+            division = divisionParent.children[1].innerText;
+        } else {
+            division = divisionParent.children[0].innerText;
+        }
 
+        const time = Number(cls.attributes.colspan.value);
+
+        return {
+            "name": table[2].innerText,
+            "week": week,
+            "period": period,
+            "teacher": table[1].innerText,
+            "credit": credit,
+            "division": division,
+            "time": time
+        }
+    })
     return {
-        "name" : table[2].innerText,
-        "week": week,
-        "period": period,
-        "teacher": table[1].innerText,
-        "credit": credit,
-        "division": division,
-        "time": time
+        "lec": lectures,
+        "title": element.querySelector("#CP").innerText
     }
-})
+}
 
-const blob = new Blob(
-    [JSON.stringify({
-        "lectures": lectures
-    })],
-    {type: "application/json"}
-);
+const download = (url, filename) => {
+    fetch(url)
+        .then(res => res.text())
+        .then(str => {
+            const doc = new DOMParser().parseFromString(str, "text/html");
+            return lectureData(doc);
+        })
+        .then(lec => {
+            const blob = new Blob(
+                [JSON.stringify({ "lectures": lec.lec })],
+                { type: "application/json" }
+            );
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            document.body.appendChild(a);
+            a.style = "display: none";
+            a.href = url;
+            a.download = filename + ".json";
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+}
 
-const url = window.URL.createObjectURL(blob);
+const grade = [
+    "1学年 前期",
+    "1学年 後期",
+    "2学年 前期",
+    "2学年 後期",
+    "3学年 前期",
+    "3学年 後期",
+    "4学年 前期",
+    "4学年 後期",
+];
 
-const a = document.createElement("a");
-document.body.appendChild(a);
-a.style = "display: none";
-a.href = url;
-// a.download = "test.json";
-a.download = document.querySelector("#CP").innerText.slice(0, -3) + ".json";
-document.body.appendChild(a);
-a.click();
-window.URL.revokeObjectURL(url);
+Array.from(document.querySelectorAll("#title"))
+    .slice(1) // 大学院時間割を除外
+    .map(title => {
+        return {
+            "table": title.nextElementSibling.querySelector("table"),
+            "faculty": title.innerText
+        }
+    })
+    .forEach(block => {
+        Array.from(block.table.querySelectorAll("tr")).forEach(tr => {
+            const tds = Array.from(tr.querySelectorAll("td"));
+            const division = tds[0].innerText;
+            const as = Array.from(tds[1].querySelectorAll("a"));
+            as.forEach((a, idx) => {
+                const url = a.href;
+                const filename = block.faculty + " " + division + " " + grade[idx];
+                download(url, filename);
+            })
+        })
+    })
